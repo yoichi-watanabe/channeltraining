af0152eb31faf8c8ea23d3bcf6979cc2415d833c
yoichi-watanabe <chip.star1120@gmail.com>
2018-04-27 00:03:31 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+func input(r io.Reader) <-chan string {
+	// TODO: チャネルを作る
+	ch := make(chan string)
+
+	go func() {
+		s := bufio.NewScanner(r)
+		for s.Scan() {
+			ch <- s.Text()
+		}
+		// TODO: チャネルを閉じる
+		close(ch)
+	}()
+	// TODO: チャネルを返す
+	return ch
+}
+
+func main() {
+	ch := input(os.Stdin)
+	for {
+		fmt.Print(">")
+		fmt.Println(<-ch)
+	}
+}
